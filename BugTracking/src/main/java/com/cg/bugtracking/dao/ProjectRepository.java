bb4d8ea9a0f72139c49c31392930a9ca96f63420
Lokesh <lokesh.manghnani@capgemini.com>
2023-01-20 16:17:23 +0530

    service and dto added

--- BugTracking/src/main/java/com/cg/bugtracking/dao/ProjectRepository.java
+++ BugTracking/src/main/java/com/cg/bugtracking/dao/ProjectRepository.java
@@ -1,5 +1,11 @@
 package com.cg.bugtracking.dao;
 
-public interface ProjectRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.bugtracking.entity.Project;
+
+@Repository
+public interface ProjectRepository  extends JpaRepository<Project, Long>{
 
 }
--- BugTracking/src/main/java/com/cg/bugtracking/service/ProjectService.java
+++ BugTracking/src/main/java/com/cg/bugtracking/service/ProjectService.java
@@ -1,5 +1,16 @@
 package com.cg.bugtracking.service;
 
+import java.util.List;
+
+import com.cg.bugtracking.entity.Project;
+import com.cg.bugtracking.exception.NoSuchProjectFoundException;
+
 public interface ProjectService {
 
+	public Project createProject(Project prj);
+	public Project getProjectById(long prjId) throws NoSuchProjectFoundException;
+	public List<Project> getAllProjects();
+	public Project updateProject(long id, Project p) throws NoSuchProjectFoundException;
+	public Project deleteProject(long id) throws NoSuchProjectFoundException;
+	
 }
