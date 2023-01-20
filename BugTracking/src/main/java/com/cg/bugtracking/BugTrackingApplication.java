61761f5290ae7db3ffa39631c9429968b9bad66d
shoaib12k <shoaib1207k@gmail.com>
2023-01-20 10:40:42 +0530

    Created project and packages

---
+++ BugTracking/src/main/java/com/cg/bugtracking/BugTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.cg.bugtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackingApplication.class, args);
+	}
+
+}
---
+++ BugTracking/src/main/java/com/cg/bugtracking/dao/ProjectRepository.java
@@ -0,0 +1,5 @@
+package com.cg.bugtracking.dao;
+
+public interface ProjectRepository {
+
+}
