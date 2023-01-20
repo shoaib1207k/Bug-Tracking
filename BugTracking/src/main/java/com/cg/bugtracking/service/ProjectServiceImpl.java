1fe9bd09f1350e7d1aba02258991d8f52e0aa2cb
Lokesh <lokesh.manghnani@capgemini.com>
2023-01-20 16:45:55 +0530

    serviceimpl

--- BugTracking/src/main/java/com/cg/bugtracking/service/ProjectServiceImpl.java
+++ BugTracking/src/main/java/com/cg/bugtracking/service/ProjectServiceImpl.java
@@ -2,11 +2,14 @@ package com.cg.bugtracking.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.bugtracking.dao.ProjectRepository;
+import com.cg.bugtracking.dto.ProjectDTO;
 import com.cg.bugtracking.entity.Project;
 import com.cg.bugtracking.exception.NoSuchProjectFoundException;
 
@@ -16,44 +19,49 @@ public class ProjectServiceImpl implements ProjectService {
 	@Autowired
 	private ProjectRepository pRepo; 
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@Override
-	public Project createProject(Project prj) {
-		return pRepo.save(prj);
+	public ProjectDTO createProject(Project prj) {
+		return modelMapper.map(pRepo.save(prj), ProjectDTO.class);
 	}
 
 	@Override
-	public Project getProjectById(long prjId) throws NoSuchProjectFoundException {
+	public ProjectDTO getProjectById(long prjId) throws NoSuchProjectFoundException {
 		Optional<Project> prj = pRepo.findById(prjId);
 		if(prj.isPresent())
-			return prj.get();
+			return modelMapper.map(prj.get(),ProjectDTO.class);
 		throw new NoSuchProjectFoundException("No Such project found");
 	}
 
 	@Override
-	public List<Project> getAllProjects() {
-		return pRepo.findAll();
+	public List<ProjectDTO> getAllProjects() {
+		return pRepo.findAll().stream().map(prj->modelMapper.map(prj, ProjectDTO.class))
+				.collect(Collectors.toList());
 
 	}
 
 	@Override
-	public Project updateProject(long id, Project p) throws NoSuchProjectFoundException {
-		Project prjToUpdate = this.getProjectById(id);
+	public ProjectDTO updateProject(long id, Project p) throws NoSuchProjectFoundException {
+		Project prjToUpdate = pRepo.findById(id).get();
+		
 		prjToUpdate.setProjId(p.getProjId());
 		prjToUpdate.setProjName(p.getProjName());
 		prjToUpdate.setProjManager(p.getProjManager());
 		prjToUpdate.setProjStatus(p.getProjStatus());
 		prjToUpdate.setBugList(p.getBugList());
-		return pRepo.save(prjToUpdate);
+		return modelMapper.map(pRepo.save(prjToUpdate), ProjectDTO.class);
 	}
 
 	@Override
-	public Project deleteProject(long id) throws NoSuchProjectFoundException {
+	public ProjectDTO deleteProject(long id) throws NoSuchProjectFoundException {
 		Optional<Project> prjToDel = pRepo.findById(id);
 		if(prjToDel.isPresent()) 
 			pRepo.delete(prjToDel.get());	
 		else
 			throw new NoSuchProjectFoundException("No project with this id");
-		return prjToDel.get();
+		return modelMapper.map(prjToDel.get(), ProjectDTO.class);
 	}
 
 	
