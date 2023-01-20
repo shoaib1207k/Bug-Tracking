de9713c587d9805452782cdccad5115bbc7319d9
shoaib12k <shoaib1207k@gmail.com>
2023-01-20 16:37:15 +0530

    Created geet all employees method for controller

--- BugTracking/src/main/java/com/cg/bugtracking/controller/EmployeeController.java
+++ BugTracking/src/main/java/com/cg/bugtracking/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.cg.bugtracking.controller;
 
-import org.modelmapper.ModelMapper;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +13,23 @@ import com.cg.bugtracking.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
-	@Autowired
-	private ModelMapper modelMapper;
 	
 	@Autowired
 	private EmployeeService empService;
 	
+	@GetMapping("/employees")
+	public ResponseEntity<List<EmployeeDTO>> getAllEmployees(){
+		try {
+			List<EmployeeDTO> empList = empService.getAllEmployees();
+			if(empList.isEmpty()) {
+				return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+			}else {
+				return new ResponseEntity<>(empList, HttpStatus.OK);
+			}
+		}catch(Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 	
 }
