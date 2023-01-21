b8c129812dfb4d145e63d78ed97464d122812158
Lokesh <lokesh.manghnani@capgemini.com>
2023-01-21 12:54:28 +0530

    controller service of project

--- BugTracking/src/main/java/com/cg/bugtracking/controller/ProjectController.java
+++ BugTracking/src/main/java/com/cg/bugtracking/controller/ProjectController.java
@@ -1,5 +1,50 @@
 package com.cg.bugtracking.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.bugtracking.dto.ProjectDTO;
+import com.cg.bugtracking.exception.NoSuchProjectFoundException;
+import com.cg.bugtracking.service.ProjectService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
 public class ProjectController {
 
+	@Autowired
+	private ProjectService prjService;
+	
+	@PostMapping("/project")
+	public ResponseEntity<ProjectDTO> createProject(@RequestBody ProjectDTO prjDTO){
+		return new ResponseEntity<>(prjService.createProject(prjDTO), HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/projects")
+	public ResponseEntity<List<ProjectDTO>> getAllProjects(){
+		return new ResponseEntity<>(prjService.getAllProjects(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/project/{id}")
+	public ResponseEntity<ProjectDTO> getProjectById(@PathVariable("id") long id) throws NoSuchProjectFoundException{
+		return new ResponseEntity<>(prjService.getProjectById(id), HttpStatus.FOUND);
+	}
+	
+	@PostMapping("/update-project/{id}")
+	public ResponseEntity<ProjectDTO> updateProject(@PathVariable("id")long id, @RequestBody ProjectDTO prjDTO) throws NoSuchProjectFoundException{
+		return new ResponseEntity<>(prjService.updateProject(id, prjDTO), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete-project/{id}")
+	public ResponseEntity<ProjectDTO> deleteProject(@PathVariable("id")long id) throws NoSuchProjectFoundException{
+		return new ResponseEntity<>(prjService.deleteProject(id), HttpStatus.OK);
+	}
 }
