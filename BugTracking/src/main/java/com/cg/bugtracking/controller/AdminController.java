8284a9ce0254b0dd737acbc17b896a7edb8d8512
Rohit Sharma <rohit.aproperemail@gmail.com>
2023-01-23 17:26:54 +0530

    new changes in admin controller

--- BugTracking/src/main/java/com/cg/bugtracking/controller/AdminController.java
+++ BugTracking/src/main/java/com/cg/bugtracking/controller/AdminController.java
@@ -15,16 +15,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.bugtracking.dto.AdminDTO;
+import com.cg.bugtracking.dto.BugDTO;
+import com.cg.bugtracking.dto.EmployeeDTO;
+import com.cg.bugtracking.dto.ProjectDTO;
 import com.cg.bugtracking.exception.NoAdminRoleFoundException;
 import com.cg.bugtracking.exception.NoSuchAdminFoundException;
 import com.cg.bugtracking.exception.NoSuchUserFoundException;
 import com.cg.bugtracking.service.AdminService;
+import com.cg.bugtracking.service.BugService;
+import com.cg.bugtracking.service.EmployeeService;
+import com.cg.bugtracking.service.ProjectService;
 
 @RestController
 public class AdminController {
 
 	@Autowired
 	private AdminService aService;
+
+	@Autowired
+	private EmployeeService empService;
+
+	@Autowired
+	private ProjectService prjService;
+
+	@Autowired
+	private BugService bugService;
 
 	@PostMapping("/admin")
 	public ResponseEntity<AdminDTO> createAdmin(@Valid @RequestBody AdminDTO adminDto)
@@ -51,6 +66,27 @@ public class AdminController {
 	@DeleteMapping("/admin/{id}")
 	public ResponseEntity<AdminDTO> deleteAdmin(@PathVariable long id) throws NoSuchAdminFoundException {
 		return new ResponseEntity<>(aService.deleteAdmin(id), HttpStatus.OK);
+	}
+
+	// employee
+
+	@PostMapping("admin/employee")
+	public ResponseEntity<EmployeeDTO> createEmployee(@Valid @RequestBody EmployeeDTO empDTO) {
+		return new ResponseEntity<>(empService.createEmployee(empDTO), HttpStatus.CREATED);
+	}
+
+	// project
+
+	@PostMapping("/admin/project")
+	public ResponseEntity<ProjectDTO> createProject(@RequestBody ProjectDTO prjDTO) {
+		return new ResponseEntity<>(prjService.createProject(prjDTO), HttpStatus.CREATED);
+	}
+
+	// bug
+
+	@PostMapping("admin/bug")
+	public ResponseEntity<BugDTO> createBug(@RequestBody BugDTO bugDTO) {
+		return new ResponseEntity<>(bugService.createBug(bugDTO), HttpStatus.CREATED);
 	}
 
 }
--- BugTracking/src/main/java/com/cg/bugtracking/service/AdminService.java
+++ BugTracking/src/main/java/com/cg/bugtracking/service/AdminService.java
@@ -3,6 +3,9 @@ package com.cg.bugtracking.service;
 import java.util.List;
 
 import com.cg.bugtracking.dto.AdminDTO;
+import com.cg.bugtracking.dto.BugDTO;
+import com.cg.bugtracking.dto.EmployeeDTO;
+import com.cg.bugtracking.dto.ProjectDTO;
 import com.cg.bugtracking.exception.NoAdminRoleFoundException;
 import com.cg.bugtracking.exception.NoSuchAdminFoundException;
 import com.cg.bugtracking.exception.NoSuchUserFoundException;
@@ -18,4 +21,16 @@ public interface AdminService {
 	public AdminDTO updateAdmin(long id, AdminDTO adminDto) throws NoSuchAdminFoundException, NoAdminRoleFoundException;
 
 	public AdminDTO deleteAdmin(long id) throws NoSuchAdminFoundException;
+
+	// employee
+
+	EmployeeDTO createEmployee(EmployeeDTO empDTO);
+
+	// project
+
+	public ProjectDTO createProject(ProjectDTO prjDTO);
+
+	// bug
+
+	public BugDTO createBug(BugDTO bugDTO);
 }
