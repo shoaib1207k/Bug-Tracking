fe15d80678a8dafbfd8319b05b51c24d8eb82a4d
Rohit Sharma <rohit.aproperemail@gmail.com>
2023-01-23 14:02:01 +0530

    minor changes added in admin, user

--- BugTracking/src/main/java/com/cg/bugtracking/dto/UserDTO.java
+++ BugTracking/src/main/java/com/cg/bugtracking/dto/UserDTO.java
@@ -1,8 +1,15 @@
 package com.cg.bugtracking.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class UserDTO {
 
+	@Min(value = 1, message = "Should be greater than 1 in user ID")
 	private long userId;
+	@NotBlank(message = "Role is required")
+	@Size(max = 15, min = 5, message = "atleast 5 characters in role")
 	private String role;
 
 	public UserDTO() {
--- BugTracking/src/main/java/com/cg/bugtracking/service/UserService.java
+++ BugTracking/src/main/java/com/cg/bugtracking/service/UserService.java
@@ -2,12 +2,12 @@ package com.cg.bugtracking.service;
 
 import java.util.List;
 import com.cg.bugtracking.dto.UserDTO;
-import com.cg.bugtracking.exception.NoAdminRoleFoundException;
+import com.cg.bugtracking.exception.IdAlreadyExistsException;
 import com.cg.bugtracking.exception.NoSuchUserFoundException;
 
 public interface UserService {
 
-	UserDTO createUser(UserDTO userDto) throws NoSuchUserFoundException, NoAdminRoleFoundException;
+	UserDTO createUser(UserDTO userDto) throws IdAlreadyExistsException;
 
 	UserDTO findById(long id) throws NoSuchUserFoundException;
 
