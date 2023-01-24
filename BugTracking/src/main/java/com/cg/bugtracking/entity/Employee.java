9661dd5759a0e5ad92f1ef3efb842c386da2f317
shoaib12k <shoaib1207k@gmail.com>
2023-01-24 17:40:06 +0530

    employee controller updated

--- BugTracking/src/main/java/com/cg/bugtracking/entity/Employee.java
+++ BugTracking/src/main/java/com/cg/bugtracking/entity/Employee.java
@@ -5,8 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Size;
+
 
 
 @Entity
