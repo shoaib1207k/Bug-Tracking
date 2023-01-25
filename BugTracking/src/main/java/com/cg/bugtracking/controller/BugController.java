d6636894209a9eed43b92799482026359358ad34
PRAKASH <PRAKASH@DESKTOP-JQN1GI7>
2023-01-25 15:11:54 +0530

    Merge branch 'Prakash-bug' of https://github.com/shoaib12k/Bug-Tracking.git into Prakash-bug

--- BugTracking/src/main/java/com/cg/bugtracking/controller/BugController.java
+++ BugTracking/src/main/java/com/cg/bugtracking/controller/BugController.java
@@ -7,9 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.bugtracking.dto.BugDTO;
@@ -19,30 +19,25 @@ import com.cg.bugtracking.exception.NotAdminException;
 import com.cg.bugtracking.service.BugService;
 
 @RestController
+@RequestMapping("/{adminId}/bug")
 public class BugController {
 	
 	@Autowired
 	private BugService bugService;
 	
-	@PostMapping("/bug/{adminId}/{id}")
-	public ResponseEntity<BugDTO> createBug(@PathVariable("id")long id, @RequestBody BugDTO bugDTO,@PathVariable("adminId")long adminId) throws NoSuchBugFoundException, NotAdminException{
-	 
-	return new  ResponseEntity<>(bugService.updateBug(bugDTO, id,adminId),HttpStatus.OK);
-   }
-	
-	@PutMapping("/bug/{adminId}/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<BugDTO> updateBug(@PathVariable("id")long id, @RequestBody BugDTO bugDTO,@PathVariable("adminId")long adminId) throws NoSuchBugFoundException, NotAdminException{
 	 
 	return new  ResponseEntity<>(bugService.updateBug(bugDTO, id,adminId),HttpStatus.OK);
    }
 	
-	@GetMapping("/bug/{adminId}/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<BugDTO> getBug(@PathVariable("id") long id,@PathVariable("adminId")long adminId) throws NoSuchBugFoundException, NotAdminException{
 		
-		return new ResponseEntity<>(bugService.getBug(id,adminId), HttpStatus.FOUND);
+		return new ResponseEntity<>(bugService.getBug(id,adminId), HttpStatus.OK);
 	}
 
-	@GetMapping("/bugs")
+	@GetMapping
 	public ResponseEntity<List<BugDTO>> getAllBug(long adminId)throws NotAdminException{
 		return new ResponseEntity<>(bugService.getAllBug(adminId), HttpStatus.OK);
 	}
@@ -53,7 +48,7 @@ public class BugController {
 	}
 	
 	
-	@DeleteMapping("/bug/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<BugDTO> deleteBug(@PathVariable("id")long id,@PathVariable("adminId")long adminId) throws NoSuchBugFoundException, NotAdminException{
 		return new ResponseEntity<>(bugService.deleteBug(id,adminId), HttpStatus.OK);
 	}
