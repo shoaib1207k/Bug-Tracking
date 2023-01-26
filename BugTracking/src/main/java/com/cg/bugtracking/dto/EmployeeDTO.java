4063282336e56008b030ba1fabc7504805ef78ce
shoaib12k <shoaib1207k@gmail.com>
2023-01-26 20:15:02 +0530

    employee dto updated

--- BugTracking/src/main/java/com/cg/bugtracking/dto/EmployeeDTO.java
+++ BugTracking/src/main/java/com/cg/bugtracking/dto/EmployeeDTO.java
@@ -2,7 +2,6 @@ package com.cg.bugtracking.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
